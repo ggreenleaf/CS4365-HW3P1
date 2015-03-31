3658ebc30ff0954de10a47d24cf4f2854b7b904f
Geoffrey <geoffrey.greenleaf@gmail.com>
2015-03-31 17:14:13 -0500

    find contradicting clauses

--- knowledge_base.py
+++ knowledge_base.py
@@ -1,24 +1,54 @@
-
-
 class KnowledgeBase:
 	def __init__(self,filename):
 		with open(filename) as f:
 			data = f.read()
 		
-		self.kb = data.split("\n")
+		self.kb = []
+		i = 1
+		for line in data.split("\n"):
+			self.kb.append((i,line,"{}"))
+			i += 1
 
 	def display(self):
-		for clause in self.kb:
-			print clause
+		for num,clause,parents in self.kb:
+			print num,clause,parents
 
 	def negate_literal(self,lit):
 		'''negates a single literal ~p ==> p or p ==> ~p''' 
-		return lit[1] if lit[0] == "~" else "~%s"%lit
+		return lit[-1] if lit[0] == "~" else "~%s"%lit
 	
 	def negate_clause(self, clause):
 		'''negates a clause and inserts into kb'''
+		length = len(self.kb) + 1
 		for literal in clause.split(" "):
 			negated = self.negate_literal(literal)
-			if negated not in self.kb:
-				self.kb.append(negated)
+			if negated not in [i[1] for i in self.kb]: 
+				self.kb.append((length,negated, "{}"))
+				length += 1
+	
+	def resolve(self,ci,cj):
+		'''returns a list of the clauses created by ci and cj'''
+		pass
+	
+	def resolution(self,a):
+		'''use resolution principle to test validy of clause'''
+		
+		if a not in [x[1] for x in self.kb]:
+			return "Clause not in Knowledge Base"
+
+		self.negate_clause(a) #negate a then put in kb
+		cur_clause = self.kb[-1] #get last clause
+		#find clause containing a negated literal in cur clause
+		
+
+
+	def find_clause(self,clause):
+		'''finds a clause in knowledge base that contains negated literal'''		
+		for l1 in clause[1].split(" "):
+			for ci in self.kb:
+				for l2 in ci[1].split(" "):
+					if self.negate_literal(l1) == l2 :
+						return clause
+		
+		return "Failure" 
 
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 from sys import argv
+from knowledge_base import KnowledgeBase as KB 
+
+
+
+kb = KB("clauses.txt")
+
+kb.resolution("~p q")
+
+
