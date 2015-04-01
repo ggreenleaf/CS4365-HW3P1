04929c0d641be5fcd8355cbe39e80f4d10ccf53c
Geoffrey <geoffrey.greenleaf@gmail.com>
2015-04-01 18:36:53 -0500

    removed inner loop in resolution 	only need to check the last clause added for contradictions 	added last_clause to check if a new clause was added to kb by resolve added part b1 infile

--- knowledge_base.py
+++ knowledge_base.py
@@ -6,6 +6,9 @@ class KnowledgeBase:
 		self.kb = []
 		i = 1
 		for line in data.split("\n"):
+			line_list = line.split(" ")
+			line_list.sort()
+			line = " ".join(line_list)
 			self.kb.append((i,line,"{}"))
 			i += 1
 
@@ -29,42 +32,83 @@ class KnowledgeBase:
 	
 	def resolve(self,ci,cj):
 		'''removes contradictions in two clauses and returns new clause'''
+		#only resolve clauses if clause not already in kb
 		union_set = ci[1].split(" ") + cj[1].split(" ")
 		literals = list(set(union_set[:]))
 		# print clauses
 		# print literals
-		print "ci:", ci[1]
-		print "cj:",  cj[1]
+		# print "ci:", ci[1]
+		# print "cj:",  cj[1]
+		# print "literals:",literals
 		for i in union_set:
 			negated = self.negate_literal(i)
 			if negated in literals:	
 				literals.remove(negated)
 				literals.remove(i)
-
+		literals.sort()
 		new_clause = (len(self.kb) + 1, " ".join(literals),"{%s %s}"%(str(cj[0]),str(ci[0])))
-		print "new clause: ", new_clause
-		return new_clause
+		if new_clause[1] not in [x[1] for x in self.kb]:
+			if new_clause[1] == "":
+				new_clause = (new_clause[0],"False",new_clause[2])
+
+			self.kb.append(new_clause)
+		
+		return new_clause 
 
 	def resolution(self,a):
-		'''use resolution principle to test validy of clause'''
+		'''use resolution principle to test validy of clause a'''
 
 		self.negate_clause(a) #start of algorithm will negate clause then instert into kb
+		kb_indx = len(self.kb) - 1
+		resolved_clauses = [] #list of parents who have already been resolved
 		while True:
-			#for i in self.kb:
-			cur_clause = self.kb[-1] #get last clause
+				# while kb_indx >= 0:
+					
+					cur_clause = self.kb[-1] #get last clause
 			#find clause containing a negated literal in cur_clause
 			#and remove contradictions
 			#for example if kb contains p ~q and ~p this is a contradiction
-			new_clause = self.resolve(cur_clause,self.find_clause(cur_clause))
-			if new_clause[1] == '': 
-			 #	new_clause[1] = "False"
-			 	self.kb.append((new_clause[0], "False",new_clause[2]))
-				break
-			elif "Failure" in new_clause[1]:
-				print "Failure"
-				break
-			else:
-				self.kb.append(new_clause)
+					self.resolve(cur_clause,self.find_clause(cur_clause))
+					if self.kb[-1][1] == "False":
+						break
+						# if resolved_clause[1] == '': #found final contradiction
+							# resolved_clause = (resolved_clause[0], "False", resolved_clause[2])
+						
+						# resolved_clauses.append(resolved_clause)
+						# kb_indx = len(self.kb) - 1 #resolved new clause re-set index to grab last clause
+					last_clause = self.kb[-1]
+
+					if last_clause == cur_clause: #if no new clause was added by resolve
+						self.kb.append((len(self.kb) + 1,"Failure","{}"))
+						break
+					# else: #if no contradiction found for cur_clause look for contradiction of previous clause
+					# 	kb_indx -= 1
+					
+
+					# if cur_clause[1] == "False":
+					# 	self.kb.append((resolved_clause[0], "False", resolved_clause[2]))
+					# 	break;
+
+					# if resolved_clause[1] == '': 
+				 # 	#	new_clause[1] = "False"
+				 # 		
+					# 	break
+					# elif "Failure" in resolved_clause[1]:
+					# 	print "Failure"
+					# 	break
+					# else:
+					# 	if not resolved_clause in resolved_clauses:
+					# 		self.kb.append(resolved_clause)				
+					# 		kb_indx = len(self.kb) - 1
+					# 	else:
+					# 		kb_indx -= 1
+
+
+
+
+
+				# if self.kb[-1][1] == "False" or kb_indx == 0:
+				# 	break
 
 
 
@@ -77,5 +121,5 @@ class KnowledgeBase:
 						return ci
 		
 
-		return ((len(self.kb) + 1, "Failure","{}"))
-
+		# return ((len(self.kb) + 1, "Failure","{}"))
+		return False
