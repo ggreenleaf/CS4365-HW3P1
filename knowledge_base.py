9f12c527f999b645f181567f75f99694b0a7280c
Geoffrey <geoffrey.greenleaf@gmail.com>
2015-03-31 22:38:59 -0500

    working with example A

--- knowledge_base.py
+++ knowledge_base.py
@@ -27,28 +27,49 @@ class KnowledgeBase:
 				length += 1
 	
 	def resolve(self,ci,cj):
-		'''returns a list of the clauses created by ci and cj'''
-		pass
-	
+		'''removes contradictions in two clauses and returns new clause'''
+		union_set = ci[1].split(" ") + cj[1].split(" ")
+		literals = union_set[:]
+		# print clauses
+		for i in union_set:
+			negated = self.negate_literal(i)
+			if negated in literals:
+				literals.remove(negated)
+				literals.remove(i)
+
+		new_clause = (len(self.kb) + 1, " ".join(literals),"{%s %s}"%(str(cj[0]),str(ci[0])))
+		return new_clause
+
 	def resolution(self,a):
 		'''use resolution principle to test validy of clause'''
 		
 		if a not in [x[1] for x in self.kb]:
 			return "Clause not in Knowledge Base"
 
-		self.negate_clause(a) #negate a then put in kb
-		cur_clause = self.kb[-1] #get last clause
-		#find clause containing a negated literal in cur clause
-		
 
+		self.negate_clause(a) #negate a then put in kb
+		for num, clause, parents in self.kb:
+			print num, clause, parents
+		
+		while True:
+			cur_clause = self.kb[-1] #get last clause
+			#find clause containing a negated literal in cur clause
+			new_clause = self.resolve(cur_clause,self.find_clause(cur_clause))
+			print new_clause[0], new_clause[1], new_clause[2]
+			if new_clause[1] == '': 
+				print "False"
+				break
+			elif new_clause == "Failure":
+				print "Failure"
+				break
+			else:
+				self.kb.append(new_clause)
 
 	def find_clause(self,clause):
-		'''finds a clause in knowledge base that contains negated literal'''		
+		'''finds a clause in knowledge base that contains a negated literal in clause'''		
 		for l1 in clause[1].split(" "):
 			for ci in self.kb:
 				for l2 in ci[1].split(" "):
 					if self.negate_literal(l1) == l2 :
-						return clause
-		
+						return ci
 		return "Failure" 
-
