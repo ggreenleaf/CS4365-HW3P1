c09c663c0b55f9619eeda3cb47e83fa8111e258b
Geoffrey <geoffrey.greenleaf@gmail.com>
2015-04-02 12:24:20 -0500

    added in and output for task files

--- main.py
+++ main.py
@@ -3,15 +3,12 @@ from knowledge_base import KnowledgeBase as KB
 
 filename = argv[1]
 clause = argv[2]
-# kb = KB("clauses.txt")
-
-# # kb.resolution("~z y")
-# # kb.display()
-
-# kb = KB("task1.in")
-# kb.resolution("LowTemp")
-# kb.display()
 
 kb = KB(filename)
-kb.resolution(clause)
-kb.display()
+
+valid = kb.resolution(clause)
+if valid:
+	kb.display()
+else:
+	kb.display()
+	print "Failure"
