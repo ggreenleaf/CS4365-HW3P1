ade35bdcd4252927e1400e0375710045ca0b418f
Geoffrey <geoffrey.greenleaf@gmail.com>
2015-04-02 11:41:40 -0500

    reworked algorithm to generate all clauses that can be resolved then adding to knowledge base"

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from sys import argv
 from knowledge_base import KnowledgeBase as KB 
 
+filename = argv[1]
+clause = argv[2]
+# kb = KB("clauses.txt")
 
+# # kb.resolution("~z y")
+# # kb.display()
 
-kb = KB("clauses.txt")
+# kb = KB("task1.in")
+# kb.resolution("LowTemp")
+# kb.display()
 
-kb.resolution("~z y")
+kb = KB(filename)
+kb.resolution(clause)
 kb.display()
-
-kb = KB("task1.in")
-kb.resolution("LowTemp")
-kb.display()
-
