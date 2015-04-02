85c78d6d5db22dd92bac56e420c365c44707ab5c
Geoffrey <geoffrey.greenleaf@gmail.com>
2015-04-02 14:58:16 -0500

    fixed problem where False was not causing resolution to return

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from sys import argv
 from knowledge_base import KnowledgeBase as KB 
 
 filename = argv[1]
-clause = argv[2]
 
-kb = KB(filename)
 
-valid = kb.resolution(clause)
-if valid:
-	kb.display()
-else:
-	kb.display()
-	print "Failure"
+kb = KB(filename) 
+
+valid = kb.resolution()
+# print valid
+# if valid:
+# 	kb.display()
+# else:
+# 	kb.display()
+# 	print "Failure"
